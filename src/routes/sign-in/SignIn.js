be820f1e61f50a2e1dec9f050eb265a22f3a489b
Jess <jessjelardo@gmail.com>
2022-12-03 20:10:29 +0800

    added sign in component as well as firebase utils and brought in firebase libraries

--- src/routes/sign-in/SignIn.js
+++ src/routes/sign-in/SignIn.js
@@ -1,7 +1,20 @@
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase'
+
+
 const SignIn = () => {
+    //note: whenever we want to make a call to a database this is going to be asynchronous
+    const logGoogleUser = async () => {
+        const {user} = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+        
+    }
+
     return (
         <div>
-            Sign in
+            <h1>Sign in Page</h1>
+            <button onClick={logGoogleUser}>
+                Sign in with Google Popup
+            </button>
         </div>
     );
 };
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -1,4 +1,15 @@
 import { initializeApp } from "firebase/app";
+// authentication
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// firestore database
+import { getFirestore, 
+  //doc - retrieves document inside firestore database
+  doc,
+  //to actually get the data
+  getDoc,
+  setDoc
+
+ } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAKEH7iT1Yb5Jghk21KnsAK9ks2WBFTKes",
@@ -9,4 +20,46 @@ const firebaseConfig = {
   appId: "1:557500382114:web:3874bd4c1ffc126149d0fb"
 };
 
-const app = initializeApp(firebaseConfig);
+//initializing config
+const firebaseApp = initializeApp(firebaseConfig);
+//to use google authentication, we need to initialize GoogleAuthProvider class we received
+const provider = new GoogleAuthProvider();
+//setting how we want googleauthprovider to behave
+provider.setCustomParameters({
+  // every time somebody interacts with our provider, we want to force them to select an account
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+//create firestore database
+export const db = getFirestore();
+
+//create user instance in the firestore db everytime a new user is authenticated
+export const createUserDocumentFromAuth = async (userAuth) => {
+  //create a reference in the doc- doc(dbinstance, collectionname, unique identifier/unique key)
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  
+  //creating an instance of a collection if it does not exist in the db
+  if(!userSnapshot.exists()){
+    const { displayName, email} = userAuth;
+    const createdAt = new Date();
+    try{
+      await setDoc(userDocRef,{
+        displayName,
+        email,
+        createdAt
+      })
+    }
+    catch(e){
+      console.log("error creating the user", e.message)
+    }
+  }
+  //if it exist return the doc
+  return userDocRef;
+  
+
+}
+    
