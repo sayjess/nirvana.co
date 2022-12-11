5a1697d7b7ee7e3c9d008bc7a333d44bffe0529f
Jess <jessjelardo@gmail.com>
2022-12-11 22:48:08 +0800

    added Link to the respective item links

--- src/components/directory-item/DirectoryItem.js
+++ src/components/directory-item/DirectoryItem.js
@@ -1,14 +1,17 @@
 import './directory-item.scss'
 
+import { Link } from 'react-router-dom'
+
 const DirectoryItem = ({category}) => {
     const {imageUrl, title} = category
     return (
         <div className="directory-item-container">
         <div  className="background-image " alt="" style={{backgroundImage: `url(${imageUrl})`}}/>
-        <div className="directory-item-body">
-          <h2>{title}</h2>
+        <Link to={`shop/${title.toLowerCase()}`} className="directory-item-body">
+          <h2>
+            {title}</h2>
           <p>Shop Now</p>
-        </div>
+        </Link>
       </div>
     )
 }
